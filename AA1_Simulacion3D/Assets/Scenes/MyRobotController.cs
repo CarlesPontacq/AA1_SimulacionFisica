c1760be05feade21b5a8fbfff5fb24000110ab0e
JoanaMorenoC <juana.moreno@estudiant.enti.cat>
2025-10-10 12:58:37 +0200

    Implement initial robot movement

--- AA1_Simulacion3D/Assets/Scenes/MyRobotController.cs
+++ AA1_Simulacion3D/Assets/Scenes/MyRobotController.cs
@@ -7,6 +7,12 @@ public class MyRobotController : MonoBehaviour
     enum RotationType { SWING, TWIST };
 
     [SerializeField] float mainAcceleration = 1.5f;
+    [SerializeField] float maxMainVelocity = 5f;
+    [SerializeField] float mainAngularAcceleration = 0.1f;
+    [SerializeField] float maxMainAngularVelocity = 0.5f;
+
+
+    float angularVelocity = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,13 +23,58 @@ public class MyRobotController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 movementDirection = Vector3.zero;
+        bool movingForward = false;
+        bool movingBackward = false;
+
         if (Input.GetKey(KeyCode.W))
         {
-            robotRB.AddForce(mainAcceleration, 0,0);
+            movementDirection = robotRB.transform.right;
+            movingForward = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            robotRB.AddForce(-mainAcceleration, 0, 0);
+            movementDirection = -robotRB.transform.right;
+            movingBackward = true;
+        }
+
+        bool tryingToRotate = false;
+        float angleToRotate = angularVelocity;
+        if (Input.GetKey(KeyCode.A) && (movingForward || movingBackward))
+        {
+            tryingToRotate = true;
+            if (movingForward)
+                angleToRotate = -angularVelocity;
+            else
+                angleToRotate = angularVelocity;
+        }
+        else if (Input.GetKey(KeyCode.D) && (movingForward || movingBackward))
+        {
+            tryingToRotate = true;
+            if (movingForward)
+                angleToRotate = angularVelocity;
+            else
+                angleToRotate = -angularVelocity;
+        }
+
+        robotRB.transform.Rotate(0, angleToRotate, 0);
+
+        if (tryingToRotate)
+        {
+            Vector3 currentVelocity = robotRB.linearVelocity;
+            robotRB.linearVelocity = Quaternion.Euler(0, angleToRotate, 0) * currentVelocity;
+
+            angularVelocity += mainAngularAcceleration;
+            if (angularVelocity > maxMainAngularVelocity)
+                angularVelocity = maxMainAngularVelocity;
+        }
+        else
+            angularVelocity = 0;
+
+        robotRB.AddForce(movementDirection * mainAcceleration);
+        if (robotRB.linearVelocity.magnitude > maxMainVelocity)
+        {
+            robotRB.linearVelocity = movementDirection * maxMainVelocity;
         }
     }
 
